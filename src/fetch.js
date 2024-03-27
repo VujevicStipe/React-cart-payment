5b4a6fd7328df57a399ae3909425c1c611d12ccc
Stipe Vujević <stipevujevic5@gmail.com>
2024-03-27 22:56:00 +0100

    Delete src directory

--- src/fetch.js
+++
@@ -1,8 +0,0 @@
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-async function fetchData() {
-  await sleep(3000);
-  return { ime: "pero", prezime: "peric", godine: 22, aktivan: false };
-}
-
-export default fetchData
